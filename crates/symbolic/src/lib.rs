de8e783e2be61e7b2050b1ef25a43fcf19fe082f
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-02 11:15:47 +0100

    Added initial symbolic crate

---
+++ crates/symbolic/src/lib.rs
@@ -0,0 +1,9 @@
+//! This crate contains all the symbolic data structures used in Merc
+//!
+//! This crate does not use any unsafe code.
+
+#![forbid(unsafe_code)]
+
+mod symbolic_lts;
+
+pub use symbolic_lts::*;
