0ffe7bd847e566a2349a5e68121de12039d1bde7
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-07 19:13:38 +0100

    Added constructor to create a parity game from a set of edges (similar to the LTSs)

--- crates/vpg/src/io_vpg.rs
+++ crates/vpg/src/io_vpg.rs
@@ -1,9 +1,11 @@
 //! Authors: Maurice Laveaux and Sjef van Loo
 
+use std::fmt;
 use std::io::BufWriter;
 use std::io::Read;
 use std::io::Write;
 
+use itertools::Itertools;
 use log::info;
 use log::trace;
 use oxidd::BooleanFunction;
@@ -33,7 +35,7 @@ use crate::VertexIndex;
 /// The format starts with a header, followed by the vertices
 ///
 /// parity <num_of_vertices>;
-/// <index> <priority> <owner> <outgoing_vertex>, <outgoing_vertex>, ...;
+/// `<index> <priority> <owner> <outgoing_vertex>, <outgoing_vertex>, ...;`
 pub fn read_vpg(manager: &BDDManagerRef, reader: impl Read) -> Result<VariabilityParityGame, MercError> {
     let mut lines = LineIterator::new(reader);
     lines.advance();
@@ -207,12 +209,13 @@ fn parse_configuration_set(
 /// Writes the given parity game to the given writer in .vpg format.
 /// Note that the reader is buffered internally using a `BufWriter`.
 pub fn write_vpg(
-    _manager: &BDDManagerRef,
+    manager_ref: &BDDManagerRef,
     writer: &mut impl Write,
     game: &VariabilityParityGame,
 ) -> Result<(), MercError> {
     let mut writer = BufWriter::new(writer);
 
+    writeln!(writer, "confs {};", FormatConfigSet::new(manager_ref, game.configuration()))?;
     writeln!(writer, "parity {};", game.num_of_vertices())?;
 
     for v in game.iter_vertices() {
@@ -220,41 +223,57 @@ pub fn write_vpg(
         let owner = game.owner(v).to_index();
 
         write!(writer, "{} {} {} ", v.value(), prio.value(), owner)?;
-        // write!(writer, "{}", game.outgoing_edges(v).format_with(", ", |fmt| {
-        //     write_configuration_set(manager_ref, write, variables, config)
-        // })
+        write!(writer, "{}", game.outgoing_edges(v).format_with(", ", |edge, fmt| {
+            fmt(&FormatConfigSet::new(manager_ref, edge.configuration()))
+        }))?;
 
-        // .map(|to| {
-        //     to.value()
-        // }).format(", "))?;
         writeln!(writer, ";")?;
     }
 
     Ok(())
 }
 
-/// Write a configuration set to its string representation.
-fn _write_configuration_set(
-    manager_ref: &BDDManagerRef,
-    write: &mut impl Write,
-    _variables: &[BDDFunction],
-    config: &BDDFunction,
-) -> Result<(), MercError> {
-    manager_ref.with_manager_shared(|_manager| -> Result<(), MercError> {
-        let choices = Vec::new();
+/// A helper structure to format configuration sets for output.
+struct FormatConfigSet<'a> {
+    manager_ref: &'a BDDManagerRef, 
+    config: &'a BDDFunction,
+}
 
-        while let Some(cube) = config.pick_cube(|_manager, _edge, index| choices[index as usize]) {
-            for value in cube {
-                match value {
-                    OptBool::True => write!(write, "1")?,
-                    OptBool::False => write!(write, "0")?,
-                    OptBool::None => write!(write, "-")?,
+impl FormatConfigSet<'_> {
+    /// Creates a new FormatConfigSet for the given manager, variables, and configuration.
+    fn new<'a>(
+        manager_ref: &'a BDDManagerRef,
+        config: &'a BDDFunction,
+    ) -> FormatConfigSet<'a> {
+        FormatConfigSet {
+            manager_ref,
+            config,
+        }
+    }
+}
+
+impl fmt::Display for FormatConfigSet<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+
+        // Use oxidd to iterate over the cubes in the BDD
+        self.manager_ref.with_manager_shared(|_manager| -> fmt::Result {
+            let choices = Vec::new();
+
+            while let Some(cube) = self.config.pick_cube(|manager, edge, index| {
+                choices[index as usize]
+            }) {
+                for value in cube {
+                    match value {
+                        OptBool::True => write!(f, "1")?,
+                        OptBool::False => write!(f, "0")?,
+                        OptBool::None => write!(f, "-")?,
+                    }
                 }
             }
-        }
 
-        Ok(())
-    })
+            Ok(())
+        })
+    }
 }
 
 #[cfg(test)]
--- crates/vpg/src/parity_game.rs
+++ crates/vpg/src/parity_game.rs
@@ -76,7 +76,8 @@ pub type VertexIndex = TagIndex<usize, VertexTag>;
 /// The strong type for a priority.
 pub type Priority = TagIndex<usize, PriorityTag>;
 
-/// Represents an explicit parity game.
+/// Represents an explicit max-priority parity game. This
+/// means that higher priority values are more significant.
 pub struct ParityGame {
     /// Stores the owner of every vertex.
     owner: Vec<Player>,
@@ -99,7 +100,7 @@ impl ParityGame {
         owner: Vec<Player>,
         priority: Vec<Priority>,
         vertices: Vec<usize>,
-        transitions_to: Vec<VertexIndex>,
+        edges_to: Vec<VertexIndex>,
     ) -> Self {
         // Check that the sizes are consistent
         debug_assert_eq!(
@@ -117,8 +118,88 @@ impl ParityGame {
             owner,
             priority,
             vertices,
-            edges_to: transitions_to,
+            edges_to,
             initial_vertex,
+        }
+    }
+
+    /// Constructs a new parity game from an iterator over edges.
+    pub fn from_edges<F, I>(
+        initial_vertex: VertexIndex,
+        owner: Vec<Player>,
+        priority: Vec<Priority>,
+        num_of_vertices: Option<usize>,
+        mut edges: F,
+    ) -> Self
+    where
+        F: FnMut() -> I,
+        I: Iterator<Item = (VertexIndex, VertexIndex)>,
+    {
+        let mut vertices = Vec::new();
+        if let Some(num_of_vertices) = num_of_vertices {
+            vertices.resize_with(num_of_vertices, Default::default);
+            debug_assert!(
+                initial_vertex.value() < num_of_vertices,
+                "Initial vertex index {} out of bounds {num_of_vertices}",
+                initial_vertex.value()
+            );
+        }
+
+        // Count the number of transitions for every state
+        let mut num_of_edges = 0;
+        for (from, to) in edges() {
+            // Ensure that the states vector is large enough.
+            if vertices.len() <= *from.max(to) {
+                vertices.resize_with(*from.max(to) + 1, || 0);
+            }
+
+            vertices[*from] += 1;
+            num_of_edges += 1;
+
+            if let Some(num_of_vertices) = num_of_vertices {
+                debug_assert!(
+                    *from < num_of_vertices && *to < num_of_vertices,
+                    "Vertex index out of bounds: from {:?}, to {:?}, num_of_vertices {}",
+                    from,
+                    to,
+                    num_of_vertices
+                );
+            }
+        }
+
+        if initial_vertex.value() >= vertices.len() {
+            // Ensure that the initial state is a valid state (and all states before it exist).
+            vertices.resize_with(initial_vertex.value() + 1, Default::default);
+        }
+
+        // Track the number of transitions before every state.
+        vertices.iter_mut().fold(0, |count, start| {
+            let result = count + *start;
+            *start = count;
+            result
+        });
+
+        // Place the transitions, and increment the end for every state.
+        let mut edges_to = vec![VertexIndex::new(0); num_of_edges];
+        for (from, to) in edges() {
+            let start = &mut vertices[*from];
+            edges_to[*start] = to;
+            *start += 1;
+        }
+
+        // Reset the offset.
+        vertices.iter_mut().fold(0, |previous, start| {
+            let result = *start;
+            *start = previous;
+            result
+        });
+
+        Self {
+            initial_vertex,
+            owner,
+            priority,
+            vertices,
+            edges_to,
         }
     }
 
