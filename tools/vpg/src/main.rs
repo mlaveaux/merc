7092247a4f851bb4f196ad09004fbda8bdeae9c7
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-08 20:32:53 +0100

    Extend the CLI

--- tools/vpg/src/main.rs
+++ tools/vpg/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::fs::read_to_string;
 use std::path::Path;
 use std::process::ExitCode;
 
@@ -6,6 +7,7 @@ use clap::Parser;
 use clap::Subcommand;
 
 use log::debug;
+use merc_syntax::UntypedStateFrmSpec;
 use merc_tools::VerbosityFlag;
 use merc_tools::Version;
 use merc_tools::VersionFlag;
@@ -18,7 +20,10 @@ use merc_vpg::guess_format_from_extension;
 use merc_vpg::read_fts;
 use merc_vpg::read_pg;
 use merc_vpg::read_vpg;
+use merc_vpg::solve_variability_zielonka;
 use merc_vpg::solve_zielonka;
+use merc_vpg::translate;
+use merc_vpg::write_vpg;
 
 #[derive(clap::Parser, Debug)]
 #[command(
@@ -64,9 +69,17 @@ struct ReachableArgs {
 /// Arguments for encoding a feature transition system into a variability parity game
 #[derive(clap::Args, Debug)]
 struct EncodeArgs {
+    /// The filename of the feature diagram
     feature_diagram_filename: String,
+
+    /// The filename of the feature transition system
     fts_filename: String,
-    vpg_filename: String,
+
+    /// The filename of the modal formula
+    formula_filename: String,
+
+    /// The variability parity game output filename
+    output: String,
 }
 
 fn main() -> Result<ExitCode, MercError> {
@@ -91,6 +104,7 @@ fn main() -> Result<ExitCode, MercError> {
                 let mut file = File::open(path)?;
                 let format =
                     guess_format_from_extension(path, args.format).ok_or("Unknown parity game file format.")?;
+
                 if format == ParityGameFormat::PG {
                     // Read and solve a standard parity game and solve it.
                     let mut time_read = timing.start("read_pg");
@@ -101,18 +115,20 @@ fn main() -> Result<ExitCode, MercError> {
                     println!("{}", solve_zielonka(&game).solution());
                     time_solve.finish();
                 } else {
+                    // Read and solve a variability parity game and solve it.
                     let manager_ref = oxidd::bdd::new_manager(2048, 1024, 1);
 
                     let mut time_read = timing.start("read_vpg");
-                    let _game = read_vpg(&manager_ref, &mut file)?;
+                    let game = read_vpg(&manager_ref, &mut file)?;
                     time_read.finish();
 
-                    // let mut time_solve = timing.start("solve_variability_zielonka");
-                    // println!("{}", solve_variability_zielonka(&manager_ref, &game).solution());
-                    // time_solve.finish();
+                    let mut time_solve = timing.start("solve_variability_zielonka");
+                    println!("{}", solve_variability_zielonka(&manager_ref, &game, false).solution());
+                    time_solve.finish();
                 }
             }
             Commands::Reachable(args) => {
+                // Read a parity game, compute its reachable part, and write it to a new file.
                 let mut file = File::open(&args.filename)?;
 
                 let mut time_read = timing.start("read_pg");
@@ -131,13 +147,27 @@ fn main() -> Result<ExitCode, MercError> {
                 merc_vpg::write_pg(&mut output_file, &reachable_game)?;
             }
             Commands::Encode(args) => {
+                // Read a feature diagram and a feature transition system, encode it into a variability parity game, and write it to a new file.
                 let manager_ref = oxidd::bdd::new_manager(2048, 1024, 1);
 
                 let mut feature_diagram_file = File::open(&args.feature_diagram_filename)?;
                 let feature_diagram = FeatureDiagram::from_reader(&manager_ref, &mut feature_diagram_file)?;
 
                 let mut fts_file = File::open(&args.fts_filename)?;
-                let _fts = read_fts(&manager_ref, &mut fts_file, &feature_diagram)?;
+                let fts = read_fts(&manager_ref, &mut fts_file, feature_diagram)?;
+
+                let formula_spec = UntypedStateFrmSpec::parse(&read_to_string(&args.formula_filename)?)?;
+                if !formula_spec.action_declarations.is_empty() {
+                    return Err(MercError::from("The formula must declare at least one action."));
+                }
+
+                if !formula_spec.data_specification.is_empty() {
+                    return Err(MercError::from("The formula must not contain a data specification."));
+                }
+
+                let vpg = translate(&manager_ref, &fts, &formula_spec.formula)?;
+                let mut output_file = File::create(&args.output)?;
+                write_vpg(&mut output_file, &vpg)?;
             }
         }
     }
