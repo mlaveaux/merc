b97cdf97fe4fc4e4e22a5bcbf75901e64662a726
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-09 20:33:43 +0100

    Allow passing errors from the apply function, and made it FnMut to allow for keeping track of information

--- crates/syntax/src/visitor.rs
+++ crates/syntax/src/visitor.rs
@@ -1,93 +1,129 @@
+use merc_utilities::MercError;
+
 use crate::StateFrm;
 
-
-
 /// Applies the given substitution function recursively to the state formula.
-/// 
+///
 /// The substitution function takes a state formula and returns an optional new
 /// formula. If it returns `Some(new_formula)`, the substitution is applied and
 /// the new formula is returned. If it returns `None`, the substitution is not
 /// applied and the function continues to traverse the formula tree.
-pub fn substitute(formula: StateFrm, substitution: &impl Fn(&StateFrm) -> Option<StateFrm>) -> StateFrm {
-    if let Some(formula) = substitution(&formula) {
+pub fn visit(
+    formula: StateFrm,
+    mut apply: impl FnMut(&StateFrm) -> Result<Option<StateFrm>, MercError>,
+) -> Result<StateFrm, MercError> {
+    visit_rec(formula, &mut apply)
+}
+
+/// See [`visit`].
+fn visit_rec(
+    formula: StateFrm,
+    apply: &mut impl FnMut(&StateFrm) -> Result<Option<StateFrm>, MercError>,
+) -> Result<StateFrm, MercError> {
+    if let Some(formula) = apply(&formula)? {
         // A substitution was made, return the new formula.
-        return formula;
+        return Ok(formula);
     }
 
     match formula {
         StateFrm::Binary { op, lhs, rhs } => {
-            let new_lhs = substitute(*lhs, substitution);
-            let new_rhs = substitute(*rhs, substitution);
-            StateFrm::Binary {
+            let new_lhs = visit_rec(*lhs, apply)?;
+            let new_rhs = visit_rec(*rhs, apply)?;
+            Ok(StateFrm::Binary {
                 op,
                 lhs: Box::new(new_lhs),
                 rhs: Box::new(new_rhs),
-            }
+            })
         }
         StateFrm::FixedPoint {
             operator,
             variable,
             body,
         } => {
-            let new_body = substitute(*body, substitution);
-            StateFrm::FixedPoint {
+            let new_body = visit_rec(*body, apply)?;
+            Ok(StateFrm::FixedPoint {
                 operator,
                 variable,
                 body: Box::new(new_body),
-            }
+            })
         }
         StateFrm::Bound { bound, variables, body } => {
-            let new_body = substitute(*body, substitution);
-            StateFrm::Bound {
+            let new_body = visit_rec(*body, apply)?;
+            Ok(StateFrm::Bound {
                 bound,
                 variables,
                 body: Box::new(new_body),
-            }
+            })
         }
         StateFrm::Modality {
             operator,
             formula,
             expr,
         } => {
-            let expr = substitute(*expr, substitution);
-            StateFrm::Modality {
+            let expr = visit_rec(*expr, apply)?;
+            Ok(StateFrm::Modality {
                 operator,
                 formula,
                 expr: Box::new(expr),
-            }
+            })
         }
         StateFrm::Quantifier {
             quantifier,
             variables,
             body,
         } => {
-            let new_body = substitute(*body, substitution);
-            StateFrm::Quantifier {
+            let new_body = visit_rec(*body, apply)?;
+            Ok(StateFrm::Quantifier {
                 quantifier,
                 variables,
                 body: Box::new(new_body),
-            }
+            })
         }
         StateFrm::DataValExprRightMult(expr, data_val) => {
-            let new_expr = substitute(*expr, substitution);
-            StateFrm::DataValExprRightMult(Box::new(new_expr), data_val)
+            let new_expr = visit_rec(*expr, apply)?;
+            Ok(StateFrm::DataValExprRightMult(Box::new(new_expr), data_val))
         }
         StateFrm::DataValExprLeftMult(data_val, expr) => {
-            let new_expr = substitute(*expr, substitution);
-            StateFrm::DataValExprLeftMult(data_val, Box::new(new_expr))
+            let new_expr = visit_rec(*expr, apply)?;
+            Ok(StateFrm::DataValExprLeftMult(data_val, Box::new(new_expr)))
         }
         StateFrm::Unary { op, expr } => {
-            let new_expr = substitute(*expr, substitution);
-            StateFrm::Unary {
+            let new_expr = visit_rec(*expr, apply)?;
+            Ok(StateFrm::Unary {
                 op,
                 expr: Box::new(new_expr),
-            }
+            })
         }
         StateFrm::Id(_, _)
         | StateFrm::True
         | StateFrm::False
         | StateFrm::Delay(_)
         | StateFrm::Yaled(_)
-        | StateFrm::DataValExpr(_) => formula,
+        | StateFrm::DataValExpr(_) => Ok(formula),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use crate::UntypedStateFrmSpec;
+
+    use super::*;
+
+    #[test]
+    fn test_visit_state_frm_variables() {
+        let input = UntypedStateFrmSpec::parse("mu X. [a]X && mu X. X && Y").unwrap();
+
+        let mut variables = vec![];
+        visit(input.formula, |frm| {
+            if let StateFrm::Id(name, _) = frm {
+                variables.push(name.clone());
+            }
+
+            Ok(None)
+        }).unwrap();
+
+        assert_eq!(variables, vec!["X", "X", "Y"]);
     }
 }
