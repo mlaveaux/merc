cc1558ca3b2c565ec1515a75a56cf9a698ac7b9d
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-11 21:30:32 +0100

    Added compute_highest and compute_bits

--- crates/symbolic/src/utility.rs
+++ crates/symbolic/src/utility.rs
@@ -11,5 +11,33 @@ fn ldd_to_bdd(storage: &mut Storage, _manager: BDDManagerRef, input: &Ldd, _bits
     unimplemented!();
 }
 
+/// Computes the highest value for every layer in the LDD
+fn compute_highest(storage: &mut Storage, ldd: &Ldd) -> Vec<usize> {
+    let mut result = vec![0; height(storage, ldd)];
+    compute_highest_rec(storage, &mut result, ldd, 0);
+    result
+}
+
+/// Helper function for compute_highest
+fn compute_highest_rec(storage: &mut Storage, result: &mut Vec<usize>, set: &LddRef<'_>, index: usize) {
+    if set == storage.empty_set() || set == storage.empty_vector() {
+        return;
+    }
+
+    let DataRef(value, right, down) = storage.get_ref(set);
+    compute_highest_rec(storage, result, &right, index);
+    compute_highest_rec(storage, result, &down, index + 1);
+
+    result[index] = result[index].max(value as usize + 1);
+}
+
+/// Computes the number of bits required to represent each element in the vector
+fn compute_bits(highest: &Vec<usize>) -> Vec<usize> {
+    highest
+        .iter()
+        .map(|&h| (usize::BITS - h.leading_zeros()) as usize)
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {}
