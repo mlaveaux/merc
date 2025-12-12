17bb06af05b925d08cff1c8ef87bd0d1e4a31f00
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-12 12:55:55 +0100

    In Sjefs work these are .svpg files.

--- crates/vpg/src/io.rs
+++ crates/vpg/src/io.rs
@@ -18,7 +18,7 @@ pub fn guess_format_from_extension(path: &Path, format: Option<ParityGameFormat>
 
     if path.extension() == Some(OsStr::new("pg")) {
         Some(ParityGameFormat::PG)
-    } else if path.extension() == Some(OsStr::new("vpg")) {
+    } else if path.extension() == Some(OsStr::new("vpg")) || path.extension() == Some(OsStr::new("svpg")) {
         Some(ParityGameFormat::VPG)
     } else {
         None
