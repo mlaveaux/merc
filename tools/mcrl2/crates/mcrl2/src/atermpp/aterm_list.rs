a1ada772544e126ef74b05557a4d366333eda873
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-13 18:51:22 +0100

    Moved the atermpp to a different module

--- tools/mcrl2/crates/mcrl2/src/atermpp/aterm_list.rs
+++ tools/mcrl2/crates/mcrl2/src/atermpp/aterm_list.rs
@@ -1,75 +1,10 @@
-use std::fmt;
 use std::marker::PhantomData;
 
-use mcrl2_sys::atermpp::ffi::aterm;
-use mcrl2_sys::atermpp::ffi::mcrl2_aterm_are_equal;
-use mcrl2_sys::atermpp::ffi::mcrl2_aterm_clone;
-use mcrl2_sys::atermpp::ffi::mcrl2_aterm_list_front;
-use mcrl2_sys::atermpp::ffi::mcrl2_aterm_list_is_empty;
-use mcrl2_sys::atermpp::ffi::mcrl2_aterm_list_tail;
-use mcrl2_sys::atermpp::ffi::mcrl2_aterm_string_to_string;
-use mcrl2_sys::atermpp::ffi::mcrl2_aterm_to_string;
-use mcrl2_sys::cxx::UniquePtr;
+use mcrl2_sys::atermpp::ffi::{mcrl2_aterm_list_front, mcrl2_aterm_list_is_empty, mcrl2_aterm_list_tail};
 
-// TODO: For this we could use the local protection set similar to the `mcrl2-rust` project.
-/// Represents a atermpp::aterm from the mCRL2 toolset.
-pub struct Aterm {
-    term: UniquePtr<aterm>,
-}
+use crate::Aterm;
 
-impl Aterm {
-    /// Creates a new `Mcrl2AtermList` from the given term.
-    pub(crate) fn new(term: UniquePtr<aterm>) -> Self {
-        Self { term }
-    }
 
-    /// Returns a reference to the underlying term.
-    pub fn get(&self) -> &aterm {
-        self.term.as_ref().expect("ATerm is null")
-    }
-}
-
-impl Clone for Aterm {
-    fn clone(&self) -> Self {
-        Aterm {
-            term: mcrl2_aterm_clone(self.get()),
-        }
-    }
-}
-
-impl PartialEq for Aterm {
-    fn eq(&self, other: &Self) -> bool {
-        mcrl2_aterm_are_equal(self.get(), other.get())
-    }
-}
-
-// The ordering is total.
-impl Eq for Aterm {}
-
-impl fmt::Debug for Aterm {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", mcrl2_aterm_to_string(&self.term))
-    }
-}
-
-/// Represents a atermpp::aterm_string from the mCRL2 toolset.
-#[derive(PartialEq, Eq)]
-pub struct AtermString {
-    term: Aterm,
-}
-
-impl AtermString {
-    /// Creates a new `ATermString` from the given term.
-    pub(crate) fn new(term: Aterm) -> Self {
-        Self { term }
-    }
-}
-
-impl fmt::Debug for AtermString {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", mcrl2_aterm_string_to_string(self.term.get()))
-    }
-}
 
 /// Represents a list of terms from the mCRL2 toolset.
 #[derive(Clone)]
@@ -102,6 +37,7 @@ impl<T: Clone + From<Aterm>> AtermList<T> {
         ATermListIter::new(self.clone())
     }
 }
+
 
 impl<T> AtermList<T> {
     /// Returns true if the list is empty.
--- tools/mcrl2/crates/mcrl2/src/lib.rs
+++ tools/mcrl2/crates/mcrl2/src/lib.rs
@@ -1,7 +1,6 @@
 //! These are Rust wrappers around the mCRL2 classes
 
 mod atermpp;
-mod busy_forbidden;
 mod data;
 mod data_expression;
 mod log;
@@ -9,7 +8,6 @@ mod pbes;
 mod visitor;
 
 pub use atermpp::*;
-pub use busy_forbidden::*;
 pub use data::*;
 pub use data_expression::*;
 pub use log::*;
